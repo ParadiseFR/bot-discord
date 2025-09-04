51ab1755bd518013311e67f37703e5ed34fc47ad
Walidoux <ma.walidkorchi@icloud.com>
2025-09-04 12:36:36 +0100

    fix(events/messageUpdate): patch logger triggering when message content changes

--- src/events/messageUpdate/logger.ts
+++ src/events/messageUpdate/logger.ts
@@ -3,26 +3,31 @@ import { EmbedBuilder, Events, Guild, TextChannel } from 'discord.js'
 import { Config, Text, event } from '../../tools'
 
 export default event(Events.MessageUpdate, async ({ client }, oldMessage, newMessage) => {
-  if (newMessage.author.bot) return
+  if (!newMessage.author.bot) {
+    if (oldMessage.content !== newMessage.content) {
+      const logChannel = client.channels.cache.get(Config.LOG_CHANNEL_ID)
 
-  const logChannel = client.channels.cache.get(Config.LOG_CHANNEL_ID)
+      if (logChannel != null && logChannel instanceof TextChannel) {
+        const embed = new EmbedBuilder()
+          .setColor('#e17055')
+          .setTitle("Modification d'un message")
+          .setDescription(
+            `${Text.bold('Auteur :')} ${Text.mention(newMessage.author.id)}\n` +
+              `${Text.bold('Ancien message :')} ${oldMessage.content}\n` +
+              `${Text.bold('Nouveau message :')} ${newMessage.content}`
+          )
+          .setTimestamp()
+          .setFooter({
+            text: (newMessage.guild as Guild).name,
+            iconURL: (newMessage.guild as Guild).iconURL() as string
+          })
 
-  if (logChannel != null && logChannel instanceof TextChannel) {
-    const embed = new EmbedBuilder()
-      .setColor('#e17055')
-      .setTitle("Modification d'un message")
-      .setDescription(
-        `${Text.bold('Auteur :')} ${Text.mention(newMessage.author.id)}\n` +
-          `${Text.bold('Ancien message :')} ${oldMessage.content}\n` +
-          `${Text.bold('Nouveau message :')} ${newMessage.content}`
-      )
-      .setTimestamp()
-      .setFooter({ text: (newMessage.guild as Guild).name, iconURL: (newMessage.guild as Guild).iconURL() as string })
-
-    try {
-      await logChannel.send({ embeds: [embed] })
-    } catch (error) {
-      console.error('Error sending message update log message:', error)
+        try {
+          await logChannel.send({ embeds: [embed] })
+        } catch (error) {
+          console.error('Error sending message update log message:', error)
+        }
+      }
     }
   }
 })
