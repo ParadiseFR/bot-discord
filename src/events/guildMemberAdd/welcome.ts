9473e454009ec52ac583b87944a543657523874e
Walidoux <ma.walidkorchi@icloud.com>
2025-09-04 12:38:13 +0100

    feat(events/guildMemberAdd): add welcome **back** message

--- src/events/guildMemberAdd/welcome.ts
+++ src/events/guildMemberAdd/welcome.ts
@@ -1,30 +1,34 @@
 import { EmbedBuilder, Events, TextChannel } from 'discord.js'
 
-import { Canvas, Config, event } from '../../tools'
-import { BOT_INSTANCE } from '../../app'
+import { Canvas, Config, Logger, event } from '../../tools'
 
 export default event(Events.GuildMemberAdd, async (_, member) => {
-  // Logger.debug(`New member: ${member.user.tag}`)
+  if (!member.user.bot) {
+    Logger.events(`Un nouveau membre a rejoint le serveur: ${member.user.tag}`)
 
-  const channel = member.guild.channels.cache.get(Config.WELCOME_CHANNEL_ID)
+    const channel = member.guild.channels.cache.get(Config.WELCOME_CHANNEL_ID)
 
-  if (channel != null && channel instanceof TextChannel) {
-    const count = (await BOT_INSTANCE.membersCount(member.guild)).toString()
-    const attachment = await Canvas.create(member, (ctx): void => {
-      ctx.fillText(`Bienvenue, ${member.user.username}!`, 250, 100)
-      ctx.fillText(`Member #${count}`, 250, 150)
-    })
+    if (channel != null && channel instanceof TextChannel) {
+      const previousJoins = member.guild.members.cache.get(member.id)?.joinedTimestamp
+      const isReturning = Boolean(previousJoins) && member.joinedTimestamp !== previousJoins
+      const title = isReturning ? 'Bon retour' : 'Bienvenue'
+      const description = isReturning ? 'heureux que vous soyez de retour ' : 'nous sommes ravis de vous accueillir'
 
-    const embed = new EmbedBuilder()
-      .setColor('#00ff00')
-      .setTitle(`Bienvenue ${member.guild.name} 🎉 !`)
-      .setDescription(`Hey ${member.user.tag}, nous sommes ravis de vous accueillir parmi nous !`) // TODO: welcome back user message
-      .setImage(`attachment://welcome.png`)
-      .setTimestamp()
+      const embed = new EmbedBuilder()
+        .setColor('#00ff00')
+        .setTitle(`${title} ${member.guild.name} 🎉 !`)
+        .setDescription(`Hey ${member.user.tag}, ${description} parmi nous !`)
+        .setImage(`attachment://welcome.png`)
+        .setTimestamp()
 
-    await channel.send({
-      embeds: [embed],
-      files: [{ attachment, name: 'welcome.png' }]
-    })
+      const attachment = await Canvas.create(member, (ctx): void => {
+        ctx.fillText(`${title}, ${member.user.username} !`, 250, 100)
+      })
+
+      await channel.send({
+        embeds: [embed],
+        files: [{ attachment, name: 'welcome.png' }]
+      })
+    }
   }
 })
