ab128228470bf5c8c707f85288b092c654944fa3
Walidoux <ma.walidkorchi@icloud.com>
2025-09-05 01:35:51 +0100

    fix(events): dispatch event log only one time + nb of handlers

--- src/tools/Event.ts
+++ src/tools/Event.ts
@@ -8,9 +8,16 @@ export const event = <T extends EventKeys>(id: T, exec: EventExec<T>): Event<T> 
   return { id, exec }
 }
 
+const eventHandlers = new Map<EventKeys, Array<EventExec<any>>>()
+
 export const registerEvents = (client: Client<true>, events: Array<Event<any>>): void => {
-  for (const event of events)
-    client.on(event.id, async (...args) => {
+  for (const event of events) {
+    if (!eventHandlers.has(event.id)) eventHandlers.set(event.id, [])
+    eventHandlers.get(event.id)?.push(event.exec)
+  }
+
+  for (const [eventId, handlers] of eventHandlers.entries()) {
+    client.on(eventId, async (...args) => {
       const props: EventProps = {
         client,
         disabled: false
@@ -18,17 +25,20 @@ export const registerEvents = (client: Client<true>, events: Array<Event<any>>):
 
       try {
         if (!props.disabled) {
-          const eventKey = Text.capitalize(event.id)
+          const eventKey = Text.capitalize(eventId)
           const eventName = Text.toUpperSnakeCase(eventKey)
 
-          await event.exec(props, ...args)
+          for (const handler of handlers) {
+            await handler(props, ...args)
+          }
 
-          Logger.events(`Event ${eventName} dispatched`)
+          Logger.events(`Event ${eventName} dispatched (${handlers.length} handlers)`)
         }
       } catch (error) {
         Logger.error('Uncaught error:', error)
       }
     })
+  }
 
   Logger.events('Events registered')
 }
